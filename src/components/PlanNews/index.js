9a0e415d8affbbaa939f5001191d28633e74ce24
lrhernandes <larachernandes@gmail.com>
2021-10-21 15:55:05 -0300

    feat (Plan News): created PlanNews heading

--- src/components/PlanNews/index.js
+++ src/components/PlanNews/index.js
@@ -2,6 +2,11 @@ import "./styles.css";
 function PlanNews() {
   return (
     <div className="plan-news section">
+      <div className="heading">
+        <span>03.</span>
+        <p className="title--md">/Plan news</p>
+        <p className="title--bg">/Cases Games</p>
+      </div>
       <p className="text-base">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam
         ultricies sollicitudin. Cras vehicula congue odio, quis lobortis leo
