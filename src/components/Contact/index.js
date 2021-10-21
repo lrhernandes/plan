f03305b5e2256b8ee8760720fb1d4ba75b707ce8
lrhernandes <larachernandes@gmail.com>
2021-10-21 18:10:31 -0300

    feat (Contact): created contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,13 +2,51 @@ import "./styles.css";
 function Contact() {
   return (
     <div className="contact section">
-      <p className="text-base">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam
-        ultricies sollicitudin. Cras vehicula congue odio, quis lobortis leo
-        gravida non. In ornare erat tortor, mattis sodales leo efficitur at. In
-        hac habitasse platea dictumst. Quisque ante tortor, egestas a lorem a,
-        tempor lacinia nisi.
-      </p>
+      <div className="contact__form">
+        <div className="heading">
+          <span>05.</span>
+          <p className="title--bg">/Contato</p>
+        </div>
+        <p className="text-base">
+          Gostou de saber mais sobre as inovações tecnológicas e também quer
+          oferecer novas experiências aos seus clientes? Então entre em contato
+          e marque um encontro real conosco para conversamos sobre as infinitas
+          possibilidades do mundo virtual.
+        </p>
+        <form>
+          <label id="name">
+            Nome:
+            <div className="input--wrapper">
+              <div className="fold"></div>
+              <input type="text"></input>
+            </div>
+          </label>
+          <div className="form--group">
+            <label id="email">
+              E-mail:
+              <div className="input--wrapper">
+                <div className="fold"></div>
+                <input type="text"></input>
+              </div>
+            </label>
+            <label id="phone">
+              Telefone:
+              <div className="input--wrapper">
+                <div className="fold"></div>
+                <input type="text"></input>
+              </div>
+            </label>
+          </div>
+          <label id="message">
+            Mensagem:
+            <div className="input--wrapper textarea">
+              <div className="fold"></div>
+              <textarea></textarea>
+            </div>
+          </label>
+        </form>
+      </div>
+      <div className="contact__cards"></div>
     </div>
   );
 }
